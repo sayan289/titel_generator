cc482df5515038345a26daeccc5eec9c6d141de6
sayan289 <sayanadhikary289@gmail.com>
2024-05-02 07:44:20 +0530

    Update test

--- src/main/java/com/example/test.java
+++ src/main/java/com/example/test.java
@@ -5,19 +5,42 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+class Prediction {
+    String label;
+    double score;
+}
 public class test {
     private static final int MAX_RETRIES = 3;
     private static final int RETRY_INTERVAL_MS = 1000;
 
     public static void main(String[] args) {
         try {
-            String apiUrl = "https://api-inference.huggingface.co/models/czearing/article-title-generator";
-            String question = "The cloud refers to a network of remote servers hosted on the internet, offering various services and resources to users on-demand. Cloud services encompass a wide range of offerings, including computing power, storage, networking, databases, analytics, and more. These services are typically provided on a pay-as-you-go basis, allowing users to scale resources up or down as needed without the hassle of managing physical infrastructure. Cloud computing enables businesses to innovate rapidly, improve agility, and reduce costs by outsourcing IT infrastructure and leveraging flexible and scalable solutions tailored to their specific requirements."
-                    +"Give the titel of the text";
+            String apiUrl = "https://api-inference.huggingface.co/models/cardiffnlp/twitter-roberta-base-dec2021-tweet-topic-multi-all";
+            String question = "Cloud computing, a revolutionary paradigm in technology, enables the storage, management, and access of data and applications over the internet. By leveraging remote servers hosted on the \"cloud,\" users can enjoy scalability, flexibility, and cost-effectiveness in their computing needs. From individual users to large enterprises, cloud computing has transformed the way we store and process information, offering convenience and efficiency in the digital age"
+                    +"Give the topic of the text";
 
             String response = sendRequest(apiUrl, question);
-            System.out.println("Response: " + response);
+//            String[]response1=response.split(",");
+            JsonArray jsonArray = JsonParser.parseString(response).getAsJsonArray();
+            Gson gson = new Gson();
+            JsonElement predictionsElement = jsonArray.get(0);
+            Prediction[] predictions = gson.fromJson(predictionsElement, Prediction[].class);
+
+// Find the label with the maximum score
+            String maxLabel = "";
+            double maxScore = Double.MIN_VALUE;
+            for (Prediction prediction : predictions) {
+                if (prediction.score > maxScore) {
+                    maxScore = prediction.score;
+                    maxLabel = prediction.label;
+                }
+            }
+
+            System.out.println("Response: " + maxLabel);
         } catch (IOException e) {
             e.printStackTrace();
         }
